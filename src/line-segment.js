46c67862bfff54603c9eca966bdc52ff1416d7d7
TravisBarryDick <travis.barry.dick@gmail.com>
2019-06-12 21:46:27 -0600

    Added LineSegment class for drawing lines in three.js

---
+++ src/line-segment.js
@@ -0,0 +1,63 @@
+// Helper class for drawing and updating line segments in three.js efficiently.
+export function LineSegment(color, width, start, end) {
+  var geometry = new THREE.BufferGeometry();
+  geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(2*3), 3));
+  var material = new THREE.LineBasicMaterial({color: color, linewidth: width});
+  this.line = new THREE.Line(geometry, material);
+
+  if (start) { this.setStart(start); }
+  if (end) { this.setEnd(end); }
+}
+
+// Returns the THREE.Line instance
+LineSegment.prototype.getLine = function() {
+  return this.line;
+}
+
+// Updates the geometry of the LineSegment
+LineSegment.prototype.updateGeo = function() {
+  this.line.geometry.attributes.position.needsUpdate = true;
+  // The geometry gets culled when its bounding sphere does not intersect with
+  // the camera's frustum. After updating the start or end points, we need to
+  // update the bounding sphere to get proper culling.
+  this.line.geometry.computeBoundingSphere();
+}
+
+// Set the starting point of the LineSegment
+LineSegment.prototype.setStart = function(v) {
+  var pb = this.line.geometry.attributes.position.array;
+  pb[0] = v.x;
+  pb[1] = v.y;
+  pb[2] = v.z;
+  this.updateGeo();
+},
+
+// Set the ending point of the LineSegment
+LineSegment.prototype.setEnd = function(v) {
+  var pb = this.line.geometry.attributes.position.array;
+  pb[3] = v.x;
+  pb[4] = v.y;
+  pb[5] = v.z;
+  this.updateGeo();
+},
+
+// Copies the starting point of the line segment into the vector v
+LineSegment.prototype.copyStart = function(v) {
+  var pb = this.line.geometry.attributes.position.array;
+  v.x = pb[0];
+  v.y = pb[1];
+  v.z = pb[2];
+}
+
+// Copies the ending point of the line segment into the vector v
+LineSegment.prototype.copyEnd = function(v) {
+  var pb = this.line.geometry.attributes.position.array;
+  v.x = pb[3];
+  v.y = pb[4];
+  v.z = pb[5];
+}
+
+// Set the visibility property of the line
+LineSegment.prototype.setVisible = function(visible) {
+  this.line.visible = visible;
+}
--- src/planet-launcher-component.js
+++ src/planet-launcher-component.js
@@ -1,4 +1,5 @@
-import AFRAME from 'aframe'
+require('aframe')
+import { LineSegment } from './line-segment'
 
 // A component that enables oculus-touch-controls to launch 'planets' (i.e.,
 // bodies)
@@ -11,14 +12,11 @@ AFRAME.registerComponent('planet-launcher', {
   init: function() {
     this.launching = false;
 
-    var geometry = new THREE.BufferGeometry();
-    geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(2*3), 3));
-    var material = new THREE.LineBasicMaterial({color: 0x00aa00, linewidth: 2});
-    this.line = new THREE.Line(geometry, material);
-    this.el.sceneEl.setObject3D('planet-launcher-line', this.line);
-
     this.planetIndex = 0;
     this.colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+    this.line = new LineSegment(0x00ff00, 2);
+    this.el.sceneEl.setObject3D('planet-launcher-line', this.line.getLine());
 
     this.tmp = new THREE.Vector3();
 
@@ -33,57 +31,30 @@ AFRAME.registerComponent('planet-launcher', {
   tick: function() {
     if (this.launching) {
       this.el.object3D.getWorldPosition(this.tmp);
-      this.setEnd(this.tmp);
+      this.line.setEnd(this.tmp);
     }
-  },
-
-  // Update the geometry buffer for the planet velocity line.
-  updateGeo: function() {
-    this.line.geometry.attributes.position.needsUpdate = true;
-    // The geometry gets culled when its bounding sphere does not intersect with
-    // the camera's frustum. After updating the start or end points, we need to
-    // update the bounding sphere to get proper culling.
-    this.line.geometry.computeBoundingSphere();
-  },
-
-  // Set the starting point of the planet velocity vector
-  setStart: function(v) {
-    var pb = this.line.geometry.attributes.position.array;
-    pb[0] = v.x;
-    pb[1] = v.y;
-    pb[2] = v.z;
-    this.updateGeo();
-  },
-
-  // Set the ending point of the planet velocity vector
-  setEnd: function(v) {
-    var pb = this.line.geometry.attributes.position.array;
-    pb[3] = v.x;
-    pb[4] = v.y;
-    pb[5] = v.z;
-    this.updateGeo();
   },
 
   // Event handler starting a planet launch action
   startLaunch: function() {
     this.launching = true;
     this.el.object3D.getWorldPosition(this.tmp);
-    this.setStart(this.tmp);
-    this.setEnd(this.tmp);
-    this.line.visible = true;
+    this.line.setStart(this.tmp);
+    this.line.setEnd(this.tmp);
+    this.line.setVisible(true);
   },
 
   // Event handler ending a planet launch action
   endLaunch: function() {
     this.launching = false;
-    this.line.visible = false;
+    this.line.setVisible(false);
     var newPlanet = document.createElement('a-body');
     let planetColor = this.colors[this.planetIndex++ % this.colors.length];
     newPlanet.setAttribute('material','color:' + planetColor);
-    var pb = this.line.geometry.attributes.position.array;
-    newPlanet.object3D.position.set(pb[0], pb[1], pb[2]);
-    var vel = new THREE.Vector3(pb[3] - pb[0], pb[4] - pb[1], pb[5] - pb[2]);
-    vel.multiplyScalar(5);
+    this.line.copyStart(newPlanet.object3D.position);
+    var vel = new THREE.Vector3();
+    this.line.copyEnd(vel);
+    vel.sub(newPlanet.object3D.position).multiplyScalar(5);
     newPlanet.setAttribute('physical-body', {mass: 0.1, 'initial-vel': vel});
     this.data.target.appendChild(newPlanet);
   }
